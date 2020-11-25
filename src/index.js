8302d03e2a979648d8f2140f209e596fc30b5d7b
Israel Lopez <israel.lpz.18@gmail.com>
2020-11-24 22:28:36 -0600

    Add optional SSL

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const path = require('path');
 const express = require('express');
 const appHttps = express();
@@ -6,44 +7,27 @@ const https = require('https');
 const fs = require('fs');
 const appHttp = express();
 
-appHttps.use(express.static(publicPath));
-
-appHttp.get('*', (req, res) => {
-    res.redirect(`https://${req.headers.host}${req.url}`)
-})
-appHttps.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-
-// Certificate
-const privateKey = fs.readFileSync(
-    '/etc/letsencrypt/live/peru-iot4.com/privkey.pem',
-    'utf8',
-);
-const certificate = fs.readFileSync(
-    '/etc/letsencrypt/live/peru-iot4.com/cert.pem',
-    'utf8',
-);
-const ca = fs.readFileSync(
-    '/etc/letsencrypt/live/peru-iot4.com/chain.pem',
-    'utf8',
-);
-
-const httpsServer = https.createServer(
-    {
-       key: privateKey,
-       cert: certificate,
-       ca,
-    },
-    appHttps,
-);
-
-
-
-httpsServer.listen(443, () => {
-   console.log('Server Https is up!');
-});
-
-appHttp.listen(80, ()=>{
-    console.log('Server Http is up!');
-})
+if(process.env.ssl === "on"){
+    appHttps.use(express.static(publicPath));
+    appHttps.get('*', (req, res) => {
+        res.sendFile(path.join(publicPath, 'index.html'));
+    });
+    // Certificate
+    const key = fs.readFileSync(process.env.private_key, 'utf8');
+    const cert = fs.readFileSync(process.env.certificate, 'utf8',);
+    const ca = fs.readFileSync(process.env.ca, 'utf8');
+    const httpsServer = https.createServer(
+        { key, cert, ca}, appHttps
+    );
+    httpsServer.listen(443, () => {
+        console.log('Server Https is up!');
+    });
+} else {
+    appHttp.use(express.static(publicPath));
+    appHttp.get('*', (req, res) => {
+        res.sendFile(path.join(publicPath, 'index.html'));
+    });
+    appHttp.listen(80, ()=>{
+        console.log('Server Http is up!');
+    })
+}
